091e9f7ec1a660d49ad89da7c8f5184b4f99c065
AKM Tasdikul Islam <akmtasdikulislam-dev@outlook.com>
2023-05-27 17:23:19 +0600

    7th Lesson: How to use Postman to send post request to api?

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // Express App Initializing Process
 
@@ -20,7 +25,37 @@ const app = express();
 // 3rd Way -> directly pass arrow function as parameter
 
 app.get("/", (req, res) => {
-  res.send("I know how to start Node ... YAY");
+  const fruit = {
+    product: "ada",
+    price: 220,
+  };
+  // res.send(
+  //   "I know how to start Node and also I learnded how to use Node Monitor ... YAY"
+  // );
+  res.send(fruit);
+});
+
+app.get("/fruits/banana", (req, res) => {
+  res.send({ fruit: "banana", quantity: 1000, price: 100000 });
+});
+
+// DYNAMIC PATH
+
+const users = ["Asad", "Moin", "Sabed", "Susmita", "Sohana", "Sabana"];
+
+app.get("/users/:id", (req, res) => {
+  // console.log(req.params.id);
+  const id = req.params.id;
+  const sort = req.query.sort;
+  // console.log(sort);
+  const name = users[id];
+  res.send({ id, name });
+});
+
+// POST
+
+app.post("/addUser", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(4200, () => console.log("Listening to port 4200"));
